963c0b0a0c4127c082b4ef3fa623dae698bda730
Sohail <thsiemailistakenbysohail@gmail.com>
2024-07-27 18:05:12 +0530

    Improve

--- src/Data/Default-Values.js
+++ src/Data/Default-Values.js
@@ -8,7 +8,7 @@ export const note=[
     {id:"M07", title:"Office", content:"First 12 notes are by default to demonstrate pagination.", time:new Date(2024,4,9)},
     {id:"M013",title:"Thought",content:"The only thning that will stop you to full fill your dreams is YOU.",time:new Date(2024,5,4)},
     {id:"M08", title:"Thought", content:"I am a MERN stack delevoper. I use React for frontend Node and Expree for backend, and MongoDB for database. ", time:new Date(2024,1,4)},
-    {id:"M09", title:"Office", content:"I like to work in office because I can get chance to meet like minded peopel, get food, and travel.",time:new Date(2024,4,19)},
+    {id:"M09", title:"Office", content:"I like to work in office because I can get chance to meet like minded peopel, get food, and travel.",time:new Date(2024,4,1)},
     {id:"M10", title:"Personal", content:"I have to take my family on dinner.",time:new Date(2024,1,10)},
     {id:"M11", title:"Office", content:"Focus on work on office time and on fun on spare time.",time:new Date(2024,2,1)},
     {id:"M12",title:"Thought", content:"Always ready to welcome new ideas.",time:new Date(2024,6,10)}
